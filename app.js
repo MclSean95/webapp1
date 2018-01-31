4d2fb19560ec683f55ba5a26492a13f4d78a0cc1
McLaughlin <samclaughlin@labs.neit.edu>
2018-01-31 12:57:36 -0500

    added db connection

--- app.js
+++ app.js
@@ -3,39 +3,64 @@ var path = require('path');
 var express = require('express');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
 
 var app = express();
 
 app.set('views', path.resolve(__dirname, 'views'));
 app.set("view engine", "ejs");
 
-var entries = [];
+//var entries = [];
 
-app.locals.entries = entries;
+//app.locals.entries = entries;
 
 app.use(logger("dev"));
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get("/", function(req, res){
-	res.render("index");
+	MongoClient.connect(url, function(err, db){
+		if(err) throw err;
+		
+		var dbObj = db.db("games");
+		
+		dbObj.collection("games").find().toArray(function(err, results){
+			console.log("Site served");
+			db.close();
+			res.render("index",{games:results});
+		});
+	});
+	
 });
 
 app.get("/new-entry", function(req, res){
 	res.render("new-entry");
 });
 
-app.post("/new-entry", function(req, res){
+app.post("/new-entry", function(req, response){
 	if(!req.body.title || !req.body.body){
-		res.status(400).send("Entries must have some text!");
+		response.status(400).send("Entries must have some text!");
 		return;
 	}
-	entries.push({
-		title: req.body.title,
-		content: req.body.body,
-		published: new Date()
+	
+	MongoClient.connect(url, function(err, db){
+		if(err) throw err;
+		
+		var dbObj = db.db("games");
+		
+		dbObj.collection("games").save(req.body, function(err, result){
+			console.log("Data saved");
+			db.close();
+			response.redirect("/");
+		});
 	});
-	res.redirect("/");
+	//entries.push({
+	//	title: req.body.title,
+	//	body: req.body.body,
+	//	published: new Date()
+	//});
+	//res.redirect("/");
 });
 
 app.use(function(req, res){
